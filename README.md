bc969c381b512b49c23e47c8e6e55b6b8f1b4634
Sarim Aleem <sarimaleem99@gmail.com>
2022-07-19 18:27:54 -0500

    basic get request in mongo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # some planning for my schema
 
+
+blog:
+should have an easy ID to index-it-by
+raw markdown
+should have a html
+comments maybe?
--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerDocument from './api/api.json';
+import type { WithId, Document } from 'mongodb'
 
 
 dotenv.config();
@@ -20,6 +20,11 @@ client.connect();
 const dbName = 'blog'
 const db = client.db(dbName);
 
+interface Post extends WithId<Document> {
+    _id: ObjectId;
+    html: string;
+    title: string;
+}
 
 app.use(
   "/api-docs",
@@ -38,6 +43,18 @@ app.get('/testDB', async (req: Request, res: Response) => {
   const elements = await collection.find({}).toArray()
   res.status(201).send(elements);
 });
+
+app.post('/publicPost', (req: Request, res: Response) => {
+  // convert markdown to html
+  // IDK the actual status
+  res.status(204).send("Created Successfully");
+})
+
+app.get('/post/:post', async (req: Request, res: Response) => {
+  const collection = db.collection("posts");
+  const post: Post = await collection.findOne({title: req.params.post}) as Post ?? {_id: new ObjectId(), html: "page not found", title: "page not found"}; 
+  res.send(post.html);
+}) 
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
